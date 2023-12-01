91a5dd6bba1d9a66ad535da1bff0bbf7645809ca
bitounu <38018844+bitounu@users.noreply.github.com>
2023-12-01 11:36:44 +0100

    myfloor should be unitiated with 1

--- day1-1.lisp
+++ day1-1.lisp
@@ -1,5 +1,5 @@
 (let ((in (open "./day1")))
-  (let ((myfloor 0)) ; Initialize myfloor
+  (let ((myfloor 1)) ; Initialize myfloor
     (loop for znak = (read-char in nil) ; Read characters in a loop
           while znak ; Continue loop until znak is non-nil (end of file)
           do (setq myfloor (if (char= znak #\() ; Compare character using char=
