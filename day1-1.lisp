26d5c12632b5545de99e1f76a063717be610e3c2
bitounu <38018844+bitounu@users.noreply.github.com>
2023-12-01 11:36:11 +0100

    gpt fix

--- day1-1.lisp
+++ day1-1.lisp
@@ -1,11 +1,10 @@
 (let ((in (open "./day1")))
-  (format t "~a~%" (read-char in))
-  (loop-for-cout dlugosc-stringa 
-  	(if  (= znak \( )
-		then
-		(+ myfloor 1)
-		else
-		(- myfloor 1)
-  	) 
-  (close in))
-
+  (let ((myfloor 0)) ; Initialize myfloor
+    (loop for znak = (read-char in nil) ; Read characters in a loop
+          while znak ; Continue loop until znak is non-nil (end of file)
+          do (setq myfloor (if (char= znak #\() ; Compare character using char=
+                                (+ myfloor 1) ; Increment if '('
+                                (- myfloor 1))) ; Decrement otherwise
+          ) ; End of loop
+    (format t "~a~%" myfloor) ; Print final value of myfloor
+    (close in))) ; Close the file
